70180dce5a3e8183059e8b19b997f6ff40511943
kapiszon0305 <kapiszon0305@gmail.com>
2021-10-21 17:26:34 +0200

    Guzik powrotu do logowania

--- app/src/main/java/pl/wikdev/feg/MainMenuActivity.java
+++ app/src/main/java/pl/wikdev/feg/MainMenuActivity.java
@@ -2,7 +2,9 @@ package pl.wikdev.feg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void powrotDoMenu(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
