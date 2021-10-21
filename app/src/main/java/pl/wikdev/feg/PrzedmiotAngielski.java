f5a76fc33836f8b374ee62be509e6a34dccad3fc
kapiszon0305 <kapiszon0305@gmail.com>
2021-10-21 18:00:05 +0200

    Dodano przedmiot ang

--- app/src/main/java/pl/wikdev/feg/PrzedmiotAngielski.java
+++ app/src/main/java/pl/wikdev/feg/PrzedmiotAngielski.java
@@ -6,22 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class PrzedmiotAngielski extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-    }
-
-    public void powrotDoMenu(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void przedmiotAng(View view) {
-        Intent intent1 = new Intent(this, PrzedmiotAngielski.class);
-        startActivity(intent1);
+        setContentView(R.layout.activity_przedmiot_angielski);
     }
     public void backtoMenu(View view) {
         Intent intent1 = new Intent(this, MainMenuActivity.class);
